bf3ea76978ede1125e712c7fdf2bd072b4cbac5e
right4leg <right4leg0727@icloud.com>
2020-06-10 20:36:59 +0900

    プログラム編集前

---
+++ Todo02/AddContoroller.swift
@@ -0,0 +1,30 @@
+//
+//  AddContoroller.swift
+//  Todo02
+//
+//  Created by hiroki kashima on 2020/06/10.
+//  Copyright © 2020 hiroki kashima. All rights reserved.
+//
+
+import UIKit
+
+class AddContoroller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
