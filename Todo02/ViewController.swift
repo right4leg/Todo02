b2f2ab2786ce47501d4ee1487d209bbea19a7962
right4leg <right4leg0727@icloud.com>
2020-06-10 20:09:39 +0900

    Initial Commit

---
+++ Todo02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo02
+//
+//  Created by hiroki kashima on 2020/06/10.
+//  Copyright © 2020 hiroki kashima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
