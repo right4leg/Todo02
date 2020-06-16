28ccf74af7e3fc8215865956ab729e28b431fb64
right4leg <right4leg0727@icloud.com>
2020-06-16 22:26:25 +0900

    完成

--- Todo02/AddContoroller.swift
+++ Todo02/AddContoroller.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
+//変数の設置
 var TodoKobetunonakami = [String]()
 
 class AddContoroller: UIViewController {
 
     @IBOutlet weak var TodoText: UITextField!
     @IBAction func Enter(_ sender: Any) {
+        //変数に入力内容を入れる
         TodoKobetunonakami.append(TodoText.text!)
+        //Enterボタンを押したらテキストフィールドを空にする
         TodoText.text = ""
+        //変数の中身をUD(UserDefoult)に追加する
         UserDefaults.standard.set(TodoKobetunonakami, forKey: "TodoList")
     }
     
--- Todo02/ViewController.swift
+++ Todo02/ViewController.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDelegate ,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TodoKobetunonakami.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let TodoCell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        TodoCell.textLabel!.text = TodoKobetunonakami[indexPath.row]
+        return TodoCell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if UserDefaults.standard.object(forKey: "TodoList") != nil{
+            TodoKobetunonakami = UserDefaults.standard.object(forKey: "TodoList") as! [String]
+        }
     }
 
 
